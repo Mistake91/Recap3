9b69c3b44d80f25eef0d791a1ffe1d89d1b2b9fc
Marc Terre <marc.terre@outlook.de>
2022-12-12 16:31:42 +0100

    test function solved wq q :q ::q

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
 );
 const searchBar = document.querySelector('[data-js="search-bar"]');
-const navigation = document.querySelector('[data-js="navigation"]');
+// const navigation = document.querySelector('[data-js="navigation"]');
 const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 let pagination = document.querySelector('[data-js="pagination"]');
@@ -13,7 +13,7 @@ let pagination = document.querySelector('[data-js="pagination"]');
 // States
 // const maxPage = 1;
 // const page = 1;
-const searchQuery = "";
+// const searchQuery = "";
 
 const fetchAPI = "https://rickandmortyapi.com/api/character";
 
@@ -33,6 +33,7 @@ export async function fetchCharacters() {
   }
 }
 
+//Zeigt ersten Character an
 fetchCharacters();
 
 async function counter() {
@@ -67,9 +68,8 @@ prevButton.addEventListener("click", () => {
 
 searchBar.addEventListener("submit", (event) => {
   event.preventDefault();
+  fetchName(event.target.elements.query.value);
 });
-
-fetchName("Agency Director");
 
 async function fetchName(name) {
   const newFetchAPI = fetchAPI + "/?name=" + name;
@@ -81,4 +81,37 @@ async function fetchName(name) {
   } catch (error) {
     console.error(error);
   }
+}
+
+//*********************************************** */
+testFunction(fetchAPI);
+
+let countCharacters = 0;
+
+async function testFunction(fetchAPI) {
+  try {
+    const response = await fetch(fetchAPI);
+    const data = await response.json();
+    countCharacters = data.info.count;
+    createArray(countCharacters);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function createArray(number) {
+  const testArray = [];
+  const newFetchAPI = fetchAPI;
+  let counter = 1;
+  while (counter < number) {
+    try {
+      const response = await fetch(newFetchAPI + `/${counter}`);
+      const data = await response.json();
+      testArray.push(data);
+    } catch (error) {
+      console.error(error);
+    }
+    counter++;
+  }
+  console.log(testArray);
 }
